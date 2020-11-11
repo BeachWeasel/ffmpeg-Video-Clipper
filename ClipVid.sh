53a3b785846721e9f58c92522a20bd2163333b40
BeachWeasel <topher.peter@gmail.com>
2020-11-10 17:07:00 -0700

    Update ClipVid.sh
    Added fixes to corner cases and an alternative to an input parser(commented out)

--- ClipVid.sh
+++ ClipVid.sh
@@ -3,7 +3,7 @@
 #https://superuser.com/questions/554620/how-to-get-time-stamp-of-closest-keyframe-before-a-given-timestamp-with-ffmpeg
 function ffnearest() {
   STIME=$2; export STIME;
-  ffprobe -read_intervals $[$STIME-25]% -select_streams v -show_frames -show_entries frame=pkt_pts_time,pict_type -v quiet "$1" |
+  ffprobe -read_intervals $[$STIME-25]%$[$STIME] -select_streams v -show_frames -show_entries frame=pkt_pts_time,pict_type -v quiet "$1" |
   awk -F= '
     /pict_type=/ { if (index($2, "I")) { i=1; } else { i=0; } }
     /pkt_pts_time/ { if (i && ($2 <= ENVIRON["STIME"])) print $2; }
@@ -20,6 +20,7 @@ Input_Method=$4
 # img="${vidname}.bmp"
 
 IFS=":" read -a Input_Start_vtc_arr <<<$Input_Start_vtc
+# Input_Start_vtc_arr=(${Input_Start_vtc//:/ })
 # echo "${Input_Start_vtc_arr[@]}"
 # echo "Number of elements: ${#Input_Start_vtc_arr[@]}"
 # echo "first entry: ${Input_Start_vtc_arr[0]}"
@@ -33,6 +34,7 @@ S_sec=${Input_Start_vtc_arr[2]}
 startvtcsec=$(bc -l <<<"$S_hours * 3600 + $S_min * 60 + $S_sec")
 
 IFS=":" read -a Input_End_vtc_arr <<<$Input_End_vtc
+# Input_End_vtc_arr=(${Input_End_vtc//:/ })
 E_hours=${Input_End_vtc_arr[0]}
 E_min=${Input_End_vtc_arr[1]}
 E_sec=${Input_End_vtc_arr[2]}
@@ -55,11 +57,17 @@ then
 		echo quick method selected
 		#quick but includes earlier time to resolve missing keyframe
 		#ffnearest "${vidname}" "${startvtcsec}"
-		key_before_startvtcsec=$(ffnearest "${vidname}" "${startvtcsec}")
+		key_before_startvtcsec=$(ffnearest "${vidname}" "${startvtcsec}" "${endvtcsec}")
 		# echo "${key_before_startvtcsec}"
 		# add in difference between startvtc and key startvtc
 		ClipLen=$(bc -l <<<"${endvtcsec} - ${key_before_startvtcsec} + 1")
 		outputVidName="${key_before_startvtcsec}_${vidname}"
+		#if key_before_startvtcsec-1 <0 then set it to zero
+		if [[ $(bc <<<"${key_before_startvtcsec} / 1") -lt 1 ]]
+		then
+			key_before_startvtcsec=1
+		fi
+		echo $(bc -l <<< "${key_before_startvtcsec} - 1")
 		ffmpeg -ss $(bc -l <<<"${key_before_startvtcsec} - 1") -i "${vidname}" -t ${ClipLen} -c:a copy -c:v copy "${outputVidName}"
 	fi
 fi
